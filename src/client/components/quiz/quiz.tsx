cf45aeba19664a8b83d9c3f006995204205cdef7
Nicholas Dupuis <ndupuis08@gmail.com>
2020-02-08 17:52:40 -0600

    Components hello world
    Organizing components - save progess

---
+++ src/client/components/quiz/quiz.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// TODO: Is there a way to simplify these imports, similar to Angular's index.ts? 
+import Question from '../question/question';
+import Summary from '../summary/summary';
+
+class Quiz extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello, this is a quiz
+                <Question></Question>
+                <Summary></Summary>
+            </div>
+        );
+    }
+}
+
+export default Quiz;
---
+++ src/client/components/summary/summary.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Summary extends React.Component {
+    render() { 
+        return (
+            <div>
+                Hello, I'm a summary
+            </div>
+        );
+    }
+}
+
+export default Summary;
